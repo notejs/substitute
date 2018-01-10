3e3a58485e5a41c7a775e7840f1596706eb54b8e
wangyoujiang <wangyoujiang@alibaba-inc.com>
2018-01-10 17:20:48 +0800

    修改为umd模块

--- substitute.js
+++ substitute.js
@@ -1,34 +1,40 @@
-(function(window, undefined){
-  // reference dojo/string( dojotoolkit.org )
-	
-	window.substitute = function(
-		/*String*/		template,
-		/*Object|Array*/map,
-		/*Function?*/	transform,
-		/*Object?*/		thisObject){
+
+(function(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(root);
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["substitute"] = factory(root);
+	else
+		root["substitute"] = factory(root);
+})(typeof self !== 'undefined' ? self : this, function(root) {
+	return function(template, map, transform, thisObject){
+		/*
+		 * summary:
+		 *		Performs parameterized substitutions on a string. Throws an
+		 *		exception if any parameter is unmatched.
+		 * template:
+		 *		a string with expressions in the form `${key}` to be replaced or
+		 *		`${key:format}` which specifies a format function. keys are case-sensitive.
+		 * map:
+		 *		hash to search for substitutions
+		 * transform:
+		 *		a function to process all parameters before substitution takes
+		 *		place, e.g. mylib.encodeXML
+		 * thisObject:
+		 *		where to look for optional format function; default to the global
+		 *		namespace
+		 */
 		
-		// summary:
-		//		Performs parameterized substitutions on a string. Throws an
-		//		exception if any parameter is unmatched.
-		// template:
-		//		a string with expressions in the form `${key}` to be replaced or
-		//		`${key:format}` which specifies a format function. keys are case-sensitive.
-		// map:
-		//		hash to search for substitutions
-		// transform:
-		//		a function to process all parameters before substitution takes
-		//		place, e.g. mylib.encodeXML
-		// thisObject:
-		//		where to look for optional format function; default to the global
-		//		namespace
-		
-		thisObject = thisObject || window;
+		thisObject = thisObject || root;
 		transform = transform || thisObject["transform"] || function(v){ return v; };
 
 		return template.replace(/\$\{([^\s\:\}]+)(?:\:([^\s\:\}]+))?\}/g, function(match, key, format){
-				var value = map[key] || ("undefined" === typeof map[key] ? match : map[key]);
-				format && (value = thisObject[format](value, key));
-				return transform(value, key).toString();
-			}); // String
+			var value = map[key] || ("undefined" === typeof map[key] ? match : map[key]);
+			format && (value = thisObject[format](value, key));
+			return transform(value, key).toString();
+		});
 	}
-}(window));
+});
+
